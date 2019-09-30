ca308ec86d3fb94f1dedc5df205e5e5a89ef6fa0
wissal <wissal.el@gmail.com>
2019-09-29 21:09:58 -0700

    Fix another bug

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors())
 
-app.get('/', (req, res) => {res.send(dataBase.users)})
+app.get('/', (req, res) => {res.send('it is working :)')})
 
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 

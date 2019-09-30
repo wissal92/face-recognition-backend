852efebf47e36fa905a9ccd582e92aad5bff769d
wissal <wissal.el@gmail.com>
2019-09-29 21:33:40 -0700

    Change the host from local host to our heroku host in database

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-animated-26828',
       user : 'postgres',
       password : 'bihter92',
       database : 'face'

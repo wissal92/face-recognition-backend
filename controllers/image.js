43eb34da1d02e021f0fe366786176f5302209687
wissal <wissal.el@gmail.com>
2019-09-29 19:56:03 -0700

    Securing our app by checking the data entered by the user in signin and register. Putting our api key in the backend

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,15 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'f2d8c6a20f994c9e9d8365c40f06a51e'
+});
+
+const handleApiCall = (req, res) => {
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => res.json(data))
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+ 
 const handleImage = (req, res, db) => {
     const {id} = req.body;
     db('users').where('id', '=', id)
@@ -10,5 +22,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-    handleImage
+    handleImage,
+    handleApiCall
 }
